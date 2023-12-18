55c9b9109e50905406161e1e362340fbf1d542ee
Krystian <krystianwasiewicz@gmail.com>
2023-12-18 15:22:49 +0100

    cos

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+    <form action="">
+        <input type="text" placeholder="Login" id="Login">
+        <input type="password" placeholder="Hasło" id="Password">
+        <div class="temp">
+            <button id="RegisterBtn">Zarejsetruj się</button>
+            <router-link to="/user">
+              <button id="LoginBtn">Zaloguj się</button>
+            </router-link>
+
+        </div>
+    </form>
+</template>
+
+<script lang="ts">
+
+export default {
+    
+}
+</script>
+
+<style src="../viewstyles/Login.css">
+</style>
+
+<style src="../viewstyles/Main.css">
+</style>
---
+++ src/views/UsertodosView.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="blur" :class="{ 'Display': isActive }">
+      <section>
+          <button id="Cancel" @click="toggleToDoDialog" >X</button>
+          <input type="text" id="Title" v-model="Title" placeholder="Tytuł">
+          <textarea name="Desc" id="Desc" cols="30" rows="10" placeholder="Opis" v-model="Desc"></textarea>
+          <label for="Deadline">Deadline</label>
+          <input type="date" name="Deadline" id="Deadline" v-model="Deadline">
+          <button id="Add" @click="addToDo">Dodaj</button>
+      </section>
+  </div>
+  <nav>
+      <router-link to="/">
+          <button id="Logout">Wyloguj</button>
+      </router-link>
+  </nav>
+  <main>
+      <table id="UserToDos">
+          <tr>
+              <th>Status</th>
+              <th>Tytuł</th>
+              <th>Opis</th>
+              <th>Data Dodania</th>
+              <th>Data Do wykonania</th>
+          </tr>
+          <tr v-for="(todo, index) in todos" :key="index">
+          <td> 
+              <select name="" id="">
+                  <option value="0">Do zrobienia</option>
+                  <option value="1">Wykoane</option>
+              </select>
+          </td>
+          <td>{{ todo.Title }}</td>
+          <td>{{ todo.Desc }}</td>
+          <td>{{ todo.DateAdded }}</td>
+          <td>{{ todo.Deadline }}</td>
+      </tr>
+      </table>
+      <p id="Placeholder">Tutaj będą twoje cele...</p>
+      <button id="AddToDo" @click="toggleToDoDialog">Dodaj cel</button>
+  </main>
+</template>
+
+<script lang="js">
+export default {
+data() {
+  return {
+    isActive: true,
+    Title: '',
+    Desc: '',
+    Deadline: '',
+    todos: [], 
+  };
+},
+methods: {
+  toggleToDoDialog() {
+    this.isActive = !this.isActive;
+  },
+  addToDo() {
+    if (this.Title && this.Desc && this.Deadline) {
+      const newTodo = {
+        Status: 'Pending',
+        Title: this.Title,
+        Desc: this.Desc,
+        DateAdded: new Date().toLocaleDateString(),
+        Deadline: this.Deadline,
+      };
+
+      this.todos.push(newTodo);
+
+      this.Title = '';
+      this.Desc = '';
+      this.Deadline = '';
+
+      this.isActive = false;
+    } else {
+      console.error('Please fill in all fields');
+    }
+  },
+}
+}
+</script>
+
+<style src="../viewstyles/Main.css">
+</style>
